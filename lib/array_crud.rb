0a6b9aaeda90fb40e06726881b997d9711d61a9c
Victor Williams <victor.williams1030@gmail.com>
2020-05-09 14:46:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,28 +1,34 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  my_array = ["bob","amanda","debby","bryan"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_array = ["bob","amanda"]
+  my_array << ("#{element}")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  my_array = ["jai","victor"]
+  my_array.unshift("#{element}")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  my_array = ["jai","victor","arrays!"]
+  my_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  my_array = ["wow","I","am","really", "learning","arrays!"]
+  my_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
+  my_array = ["wow","I","am","really","learning","arrays!"]
+  .index(2)
   
 end
 
